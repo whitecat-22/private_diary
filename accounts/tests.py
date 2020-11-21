93ae305bc6d9494aa6eb981a9c8ec66691ccef93
whitecat-22 <gattinobianco22@gmail.com>
2020-11-21 19:29:36 +0900

    .gitignore modified

---
+++ accounts/tests.py
@@ -0,0 +1,32 @@
+# from django.test import TestCase
+
+# Create your tests here.
+from django.test import LiveServerTestCase
+from django.urls import reverse_lazy
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+class TestLogin(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(executable_path='<Chromeドライバ配置場所>')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        # ログインページを開く
+        self.selenium.get('http://localhost:8000' + str(reverse_lazy('account_login')))
+
+        # ログイン
+        username_input = self.selenium.find_element_by_name("login")
+        username_input.send_keys('<ユーザー登録済みのメールアドレス>')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('<ログインパスワード>')
+        self.selenium.find_element_by_class_name('btn').click()
+
+        # ページタイトルの検証
+        self.assertEquals('日記一覧 | Private Diary', self.selenium.title)
