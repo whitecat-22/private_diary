70c51c70c3ad98bd1b95639a3504522b03e7cc2b
_whitecat_22 <74803154+whitecat-22@users.noreply.github.com>
2020-11-25 10:43:41 +0900

    本番運用環境の再整備のため、一旦、設定をHTTP用に戻す

--- private_diary/settings_dev.py
+++ private_diary/settings_dev.py
@@ -4,7 +4,7 @@ from .settings_common import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []        # '127.0.0.1' or 'localhost' を直接指定しないと上手く行かない
+ALLOWED_HOSTS = ['*']        # '127.0.0.1' or 'localhost' を直接指定しないと上手く行かない
 
 
 DATABASES = {
